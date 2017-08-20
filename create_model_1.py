47d7386af2443d45b05fcd5171cf7cfc391542c2
hyojeonglee <hyon2224@naver.com>
2017-08-20 19:13:58 +0900

    modify learning time to default

--- create_model_1.py
+++ create_model_1.py
@@ -55,7 +55,7 @@ https://www.tensorflow.org/versions/master/how_tos/variables/index.html
 """
 with tf.Session() as sess:
     sess.run(init_op)
-    for i in range(5000):
+    for i in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
         
