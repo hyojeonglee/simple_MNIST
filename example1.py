5f6e2a7225558e81a5014384c8caa205a443201f
hyojeonglee <hyon2224@naver.com>
2017-09-18 17:45:46 +0900

    modify example1

--- example1.py
+++ example1.py
@@ -1,6 +1,6 @@
 # simple example 1
 
-import tensorflow as tensorflow
+import tensorflow as tf
 
 node1 = tf.constant(3.2, tf.float32)
 node2 = tf.constant(4.3, tf.float32)
@@ -8,7 +8,7 @@ node3 = tf.add(node1, node2)
 
 sess = tf.Session()
 
-print(sess.run([node1, node2])) // [3.2, 4.3]
-print(sess.run(node3)) // 7.5
+print(sess.run([node1, node2])) # [3.2, 4.3]
+print(sess.run(node3)) # 7.5
 
 
